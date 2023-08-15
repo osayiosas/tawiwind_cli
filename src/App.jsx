62411b73905ec87d1f856873de4a8b94dc326320
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-08-15 12:17:42 +0100

    trying tailwind with the cli

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './Card';
+
+const App = () => {
+  return (
+    <>
+
+    <Card />
+      <section className=" m-12 h-screen bg-twitter-blue flex items-center text-center text-3xl  ">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+          voluptas in reprehenderit placeat quisquam modi.
+        </p>
+      </section>
+
+      
+       
+    </>
+  );
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">this is acard</div>
+    </div>
+  );
+}
+
+export default Card
